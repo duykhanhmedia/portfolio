597d474ad394b35c6a4470de74ea0d8e217f4a88
Duy Khanh <nguyenduykhanh15101997@gmail.com>
2025-02-21 03:40:14 +0700

    fffff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/", // hoặc base: "/portfolio/" nếu deploy lên GitHub Pages
   plugins: [react()],
+  base: "/portfolio/", // hoặc base: "/portfolio/" nếu deploy lên GitHub Pages
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
 });
