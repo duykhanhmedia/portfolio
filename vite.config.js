e5f7f96bbee2740bccd03811974f20a5cdcb5f6a
Duy Khanh <nguyenduykhanh15101997@gmail.com>
2025-02-21 03:46:02 +0700

    gggg

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], // Hỗ trợ JSX cho Vite,
-  base: "./",
+  plugins: [react()],
+  base: "/portfolio/",
   resolve: {
-    extensions: ['.js', '.jsx'], // Cho phép import không cần đuôi file
+    extensions: ['.js', '.jsx'],
     alias: [{ find: "@", replacement: "/src" }],
   }
 });
